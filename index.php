cca7c459622dbe73ed4c846d55858cf8b3325dcf
Michelle <michelleashwini@mcruncher.com>
2017-03-09 11:34:43 +0800

    development #5m Added bootstrap.min.css and bootstrap.min.js. Also created worshipsongs-lyrics.css file.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tag Generator</title>
+        <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" >
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300, 400' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <h1>Test Heading</h1>
+
+        <!--Scripts-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="scripts/bootstrap.min.js"></script>
+    </body>
+</html>
